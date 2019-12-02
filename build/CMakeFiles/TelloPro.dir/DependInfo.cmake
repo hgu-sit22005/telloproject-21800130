a49dc5b59ec68d226f7df74139521ac2d558e721
21800130 <21800130@handong.edu>
2019-12-02 11:38:30 +0900

    tello

---
+++ build/CMakeFiles/TelloPro.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/data/TelloProject/src/back.cpp" "/data/TelloProject/build/CMakeFiles/TelloPro.dir/src/back.cpp.o"
+  "/data/TelloProject/src/ccw.cpp" "/data/TelloProject/build/CMakeFiles/TelloPro.dir/src/ccw.cpp.o"
+  "/data/TelloProject/src/cw.cpp" "/data/TelloProject/build/CMakeFiles/TelloPro.dir/src/cw.cpp.o"
+  "/data/TelloProject/src/down.cpp" "/data/TelloProject/build/CMakeFiles/TelloPro.dir/src/down.cpp.o"
+  "/data/TelloProject/src/flip.cpp" "/data/TelloProject/build/CMakeFiles/TelloPro.dir/src/flip.cpp.o"
+  "/data/TelloProject/src/forward.cpp" "/data/TelloProject/build/CMakeFiles/TelloPro.dir/src/forward.cpp.o"
+  "/data/TelloProject/src/land.cpp" "/data/TelloProject/build/CMakeFiles/TelloPro.dir/src/land.cpp.o"
+  "/data/TelloProject/src/left.cpp" "/data/TelloProject/build/CMakeFiles/TelloPro.dir/src/left.cpp.o"
+  "/data/TelloProject/src/python_interface.cpp" "/data/TelloProject/build/CMakeFiles/TelloPro.dir/src/python_interface.cpp.o"
+  "/data/TelloProject/src/right.cpp" "/data/TelloProject/build/CMakeFiles/TelloPro.dir/src/right.cpp.o"
+  "/data/TelloProject/src/takeoff.cpp" "/data/TelloProject/build/CMakeFiles/TelloPro.dir/src/takeoff.cpp.o"
+  "/data/TelloProject/src/up.cpp" "/data/TelloProject/build/CMakeFiles/TelloPro.dir/src/up.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/include/python3.6m"
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
